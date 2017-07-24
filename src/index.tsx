327916fddb6edb3cba8e66636f456d1aeeb63b4f
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-07-24 22:41:02 +0100

    props

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,40 @@
 import * as React from 'react';
 import { DeepStorage, Path, DeepSubscription, parsePaths, stringOrNumber } from "deep-storage";
 
-export const deep = <State extends {}>(storage: DeepStorage<State>, paths: { [key: string]: Path | stringOrNumber }) => <P extends {}>(BaseComponent: React.ComponentType<P>) => {
-    const parsedPaths = parsePaths(paths);
-    return class extends React.Component<P, {}> {
-        subscription: DeepSubscription;
-        componentDidMount() {
-            this.subscription = storage.subscription((...args: any[]) => {
-                this.forceUpdate();
-            });
-            for (let key in paths) {
-                this.subscription.subscribeTo(...parsedPaths[key]);
-            }
-        }
-        componentWillUnmount() {
-            this.subscription && this.subscription.cancel();
-        }
-        shouldComponentUpdate(nextProps: P, nextState: {}) {
-            const nextPropsAny: any = nextProps;
-            for (let key in parsedPaths) {
-                if (nextPropsAny[key] !== storage.stateIn(...parsedPaths[key])) {
-                    return true;
+export const deep = <State extends {}, P>(
+    storage: DeepStorage<State>,
+    paths: { [key: string]: Path | stringOrNumber },
+    props: { [key: string]: Partial<P> }) => (BaseComponent: React.ComponentType<P>) => {
+        const parsedPaths = parsePaths(paths);
+        return class extends React.Component<P, {}> {
+            subscription: DeepSubscription;
+            componentDidMount() {
+                this.subscription = storage.subscription((...args: any[]) => {
+                    this.forceUpdate();
+                });
+                for (let key in paths) {
+                    this.subscription.subscribeTo(...parsedPaths[key]);
                 }
             }
-            return false;
-        }
-        render() {
-            const anyProps: any = this.props;
-            const newProps: any = {...anyProps};
-            for (let key in parsedPaths) {
-                newProps[key] = storage.stateIn(...parsedPaths[key])
+            componentWillUnmount() {
+                this.subscription && this.subscription.cancel();
             }
-            return <BaseComponent {...newProps} />;
-        }
-    };
-}
+            shouldComponentUpdate(nextProps: P, nextState: {}) {
+                const nextPropsAny: any = nextProps;
+                for (let key in parsedPaths) {
+                    if (nextPropsAny[key] !== storage.stateIn(...parsedPaths[key])) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            render() {
+                const anyProps: any = this.props;
+                const newProps: any = { ...anyProps };
+                for (let key in parsedPaths) {
+                    newProps[key] = storage.stateIn(...parsedPaths[key])
+                }
+                return <BaseComponent {...newProps} />;
+            }
+        };
+    }
