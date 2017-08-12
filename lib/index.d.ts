d8be101cd6933b7d80c76e53697f8ac83d91a042
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-08-12 11:46:32 +0100

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,32 +1,32 @@
 /// <reference types="react" />
 import * as React from 'react';
 import { DeepStorage, DeepSubscription, UsesDeepStorage } from "deep-storage";
-export declare const connect: <State extends {}, P, K extends keyof P>(deepProps: {
-    [key: string]: DeepStorage<State, {}> | UsesDeepStorage<State>;
+export declare const connect: <PropsType extends {}, Key extends keyof PropsType>(deepProps: {
+    [key in Key]: DeepStorage<PropsType[Key], {}> | UsesDeepStorage<PropsType[Key]>;
 }, ownProps?: {
-    [key in K]: P[K];
-}) => (BaseComponent: React.ComponentType<P>) => {
-    new (props?: P, context?: any): {
+    [key in Key]: PropsType[Key];
+}) => (BaseComponent: React.ComponentType<PropsType>) => {
+    new (props?: PropsType, context?: any): {
         subscription: DeepSubscription;
         componentDidMount(): void;
         componentWillUnmount(): void;
-        shouldComponentUpdate(nextProps: P, nextState: {}): boolean;
+        shouldComponentUpdate(nextProps: PropsType, nextState: {}): boolean;
         render(): JSX.Element;
-        setState<K extends never>(f: (prevState: {}, props: P) => Pick<{}, K>, callback?: () => any): void;
+        setState<K extends never>(f: (prevState: {}, props: PropsType) => Pick<{}, K>, callback?: () => any): void;
         setState<K extends never>(state: Pick<{}, K>, callback?: () => any): void;
         forceUpdate(callBack?: () => any): void;
         props: Readonly<{
             children?: React.ReactNode;
-        }> & Readonly<P>;
+        }> & Readonly<PropsType>;
         state: Readonly<{}>;
         context: any;
         refs: {
             [key: string]: React.ReactInstance;
         };
         componentWillMount?(): void;
-        componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
-        componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<{}>, nextContext: any): void;
-        componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<{}>, prevContext: any): void;
+        componentWillReceiveProps?(nextProps: Readonly<PropsType>, nextContext: any): void;
+        componentWillUpdate?(nextProps: Readonly<PropsType>, nextState: Readonly<{}>, nextContext: any): void;
+        componentDidUpdate?(prevProps: Readonly<PropsType>, prevState: Readonly<{}>, prevContext: any): void;
     };
 };
 export default connect;
