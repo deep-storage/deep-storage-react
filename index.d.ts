0970d57585be425221febd5fb8d3a1efecb0596e
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-08-12 12:17:22 +0100

    build

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="react" />
 import * as React from 'react';
 import { DeepStorage, UsesDeepStorage } from "deep-storage";
+export interface ComponentCreator<P = {}> {
+    component: React.ComponentType<P>;
+}
 export declare const connect: <PropsType extends {}, Key extends keyof PropsType>(deepProps: {
     [key in Key]: DeepStorage<PropsType[Key], {}> | UsesDeepStorage<PropsType[Key]>;
 }, ownProps?: {
