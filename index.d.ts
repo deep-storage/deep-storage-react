27b3961a1c905c68e0cd2f8f42b66d5127c7031f
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-08-12 12:08:05 +0100

    root

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { DeepStorage, UsesDeepStorage } from "deep-storage";
+export declare const connect: <PropsType extends {}, Key extends keyof PropsType>(deepProps: {
+    [key in Key]: DeepStorage<PropsType[Key], {}> | UsesDeepStorage<PropsType[Key]>;
+}, ownProps?: {
+    [key in Key]: PropsType[Key];
+}) => (BaseComponent: React.ComponentType<PropsType>) => {
+    new (props?: PropsType, context?: any): {
+        render(): JSX.Element;
+        setState<K extends never>(f: (prevState: {}, props: PropsType) => Pick<{}, K>, callback?: () => any): void;
+        setState<K extends never>(state: Pick<{}, K>, callback?: () => any): void;
+        forceUpdate(callBack?: () => any): void;
+        props: Readonly<{
+            children?: React.ReactNode;
+        }> & Readonly<PropsType>;
+        state: Readonly<{}>;
+        context: any;
+        refs: {
+            [key: string]: React.ReactInstance;
+        };
+        componentWillMount?(): void;
+        componentDidMount?(): void;
+        componentWillReceiveProps?(nextProps: Readonly<PropsType>, nextContext: any): void;
+        shouldComponentUpdate?(nextProps: Readonly<PropsType>, nextState: Readonly<{}>, nextContext: any): boolean;
+        componentWillUpdate?(nextProps: Readonly<PropsType>, nextState: Readonly<{}>, nextContext: any): void;
+        componentDidUpdate?(prevProps: Readonly<PropsType>, prevState: Readonly<{}>, prevContext: any): void;
+        componentWillUnmount?(): void;
+    };
+};
+export default connect;
