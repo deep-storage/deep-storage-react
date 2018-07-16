3bd43ce984d3dc9a3bb040679169a896e5eae892
Jamie McCrindle <jamiemccrindle@gmail.com>
2018-07-16 21:10:32 +0100

    5

---
+++ src/connect.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import { DeepStorage, Path, Subscriber } from "deep-storage";
+
+export const connect = <
+  ConnectedComponentPropType extends {},
+  BaseComponentPropsType extends {}
+>(
+  deepProps: {
+    [key in keyof BaseComponentPropsType]?: DeepStorage<
+      BaseComponentPropsType[key]
+    >
+  },
+  ownProps?: {
+    [key in keyof BaseComponentPropsType]?: BaseComponentPropsType[key]
+  }
+) => (
+  BaseComponent: React.ComponentType<BaseComponentPropsType>
+): React.ComponentType<ConnectedComponentPropType> => {
+  const deepPropsKeys = Object.keys(
+    deepProps
+  ) as (keyof BaseComponentPropsType)[];
+
+  // if no deep props specified, just return regular component
+  if (deepPropsKeys.length === 0)
+    return class extends React.Component<ConnectedComponentPropType, {}> {
+      render() {
+        return <BaseComponent {...ownProps || {}} {...this.props} />;
+      }
+    };
+
+  return class extends React.Component<ConnectedComponentPropType, {}> {
+    subscriber: Subscriber = new Subscriber();
+    componentDidMount() {
+      for (const deepPropsKey of deepPropsKeys) {
+        const deepPropsValue = deepProps[deepPropsKey];
+        if (deepPropsValue) {
+          deepPropsValue.addSubscriber(this.subscriber);
+        }
+      }
+    }
+    componentWillUnmount() {
+      for (const deepPropsKey of deepPropsKeys) {
+        const deepPropsValue = deepProps[deepPropsKey];
+        if (deepPropsValue) {
+          deepPropsValue.removeSubscriber(this.subscriber);
+        }
+      }
+    }
+    shouldComponentUpdate(
+      nextProps: ConnectedComponentPropType,
+      nextState: {}
+    ) {
+      const nextPropsAny: any = nextProps;
+      for (let key in nextPropsAny) {
+        const deepPropsValue = deepProps[key as keyof BaseComponentPropsType];
+        if (deepPropsValue) {
+          if (deepPropsValue.state !== nextPropsAny[key]) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+    render() {
+      const anyProps: any = this.props;
+      const newProps: any = Object.assign({}, anyProps, ownProps || {});
+      for (let deepPropsKey in deepProps) {
+        const value = deepProps[deepPropsKey];
+        if (value) {
+          newProps[deepPropsKey] = value.state;
+        }
+      }
+      return <BaseComponent {...newProps} />;
+    }
+  };
+};
